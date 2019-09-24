3bc248f93b705c23ec758d1cfa79e04312dc8637
Xuan Zhu <xuan.zhu@healthtap.com>
2019-09-23 22:39:49 -0700

    add the rudimentary math operations in the front end and back end

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,33 @@ router.get('/', async (ctx, next) => {
   })
 });
 
+//navigate to math page, render the math template
+router.get('/math', async (ctx, next) => {
+  await ctx.render('math', {
+    title: 'Math Expression'
+  });
+});
+
+//provider an entry point in the server side to do the calculation
+router.post('/calculate', (ctx, next) => {
+  //get the expression
+  const expression = ctx.request.body.expression;
+  //use eval to do the calculation
+  try {
+    //eval function is not very safe, so do validation for the user input first
+    //the user input should only contain + - * / ( ) and space
+    const filtered = expression.replace(/[\d\+\-\*\/\(\)\s]*/g, "");
+
+    if (filtered.length !== 0) {
+      throw 'invalid expression';
+    }
+    //after the validation from user input, we could use eval to calculate the result:
+    ctx.status = 200;
+    ctx.message = eval(expression).toString();
+  } catch (e) {
+    ctx.status = 200;
+    ctx.message = 'invalid expression, please input only + - * / and numbers';
+  }
+});
+
 module.exports = router;
